c6c8365563282821ed77d0dde88d20979a4d1e7e
uleevDanPMI <165877975+uleevDanPMI@users.noreply.github.com>
2024-05-15 12:58:09 +0500

    Add files via upload
    новые файлы сюда добавил main6 и остальные

---
+++ Main6.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        
+      
+        for (Men men : MenArray) {
+            System.out.println( men.getName() + Men.getFio());
+        }
+
+        
+        ArrayBs[0].Find(B[]Bs, int QuantityOfBs, String name) {
+        for (int i = 0; i <QuantityOfBs; i++) {
+            if (Bs[i].getName().equals(name)) {
+                System.out.println("Person found: " + Bs[i]);
+                return;
+            }
+            
+        }
+    }
+}
+}
